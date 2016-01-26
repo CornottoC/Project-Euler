ecce2ff809c10b14bc31beacd984b8b3c85c3be8
ata <alexander.karell@gmail.com>
2016-01-26 11:21:12 +0200

    adding working and trivial solution to Eulers 1st problem

--- Euler1/Euler1/Euler1.cpp
+++ Euler1/Euler1/Euler1.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include "euler1Simple.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	euler1_simpl e(3,5,1000);
+	cout << e.result();
+	char c;
+	while (c=getchar())
+	{
+		if (c == 'q')break;
+	}
     return 0;
 }
 
