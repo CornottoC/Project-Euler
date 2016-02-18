59fe2b4552896db8077c6623ec4c4684b024b6e8
ata <alexander.karell@gmail.com>
2016-02-18 14:38:00 +0200

    Solution 23 project created, not started

---
+++ Euler1/Euler23/Euler23.cpp
@@ -0,0 +1,17 @@
+// Euler23.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+
+
+	char c;
+	while (c = getchar())
+	{
+		if (c == 'q')break;
+	}
+	return 0;
+}
