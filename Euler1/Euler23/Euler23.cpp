7902515a2bf023b0ca16deb8acbb92d386282e4e
ata <alexander.karell@gmail.com>
2016-02-19 13:33:20 +0200

    Adding solution 23, brute force but right result. Run time about 10min on Core-m, so very not optimal. Check for prime factorisation algoritm. Factorisation is the slow thing in this loop

--- Euler1/Euler23/Euler23.cpp
+++ Euler1/Euler23/Euler23.cpp
@@ -3,11 +3,15 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "simpleEuler23.h"
 
 int main()
 {
+	simpleEuler23 e;
 
-
+	//std::cout << std::boolalpha << e.isAbundant(348)<<std::endl;
+	//std::cout << std::boolalpha << e.isSumOfTwoAbundant(360);
+	e.sumOfTheNumbersNotSums();
 	char c;
 	while (c = getchar())
 	{
